1bd158ae898233b25c0b7895743da4f219b0edee
IT21221514 Tharuka <99517876+IT21221514@users.noreply.github.com>
2022-02-16 13:27:14 +0000

    tute 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,21 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mark1;
+	int mark2;
+	
+	printf("enter mark1:");
+	scanf("%d",&mark1);
+	printf("enter mark2:");
+	scanf("%d",&mark2);
+	 
+	 int sum = mark1+mark2;
+	 int avarage= sum/2.0f;
+	 
+	 printf("sum is %d\n", sum);
+	 printf(" avarage is %d", avarage);
+
+
   return 0;
 }
 
